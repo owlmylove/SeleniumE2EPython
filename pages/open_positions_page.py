c2e9dd648e42ad0d7ab1c3e78a85e0e39f3cf6fb
Lilit Hevorhian <lilitturbizinfo@gmail.comm>
2024-10-14 00:44:56 +0200

    added more pages

--- pages/open_positions_page.py
+++ pages/open_positions_page.py
@@ -1,36 +1,41 @@
 from selenium.webdriver.common.by import By
 
 
-class QualityAssurancePage:
+class OpenPositionsPage:
     def __init__(self, driver):
         self.driver = driver
-        self.careers_quality_assurance_url = "https://useinsider.com/careers/quality-assurance/"
-        self.see_allQaJobs_button = (By.CLASS_NAME, ".btn-outline-secondary")  # See all QA jobs
+        self.find_jobs_button_Careers = (By.XPATH, "//*[@id='page-head']/div/div/div[1]/div/div/a")
         self.filter_location = (By.ID, "[id='select2-filter-by-location-container']")  # //*[@id="select2-filter-by-location-container"]
         self.filter_department = (By.ID, "[id='select2-filter-by-department-container']")  # //*[@id="select2-filter-by-department-container"]
         self.location_Turkey = (By.ID, "[id='select2-filter-by-location-result-y2n4-Istanbul, Turkey']")  # //*[@id="select2-filter-by-location-result-y2n4-Istanbul, Turkey"]
-        self.department_qualityAssurance = (By.ID, "[id='select2-filter-by-department-result-2cey-Quality Assurance'']")
+        self.department_qualityAssurance = (By.ID, "[id='select2-filter-by-department-result-2cey-Quality Assurance'']")  # //*[@id="select2-filter-by-department-result-2cey-Quality Assurance"]
         self.positions_list = (By.ID, "[id='jobs-list'']")
-        self.position_title = (By.CLASS_NAME, ".position-title")  # By.XPATH, "//*[text()='Quality Assurance']"
+        self.position_title = (By.CLASS_NAME, ".position-title")  # Quality Assurance
         self.position_department = (By.CLASS_NAME, ".position-department")  # Quality Assurance
         self.position_location = (By.CLASS_NAME, ".position-location")  # Istanbul, Turkey
+        self.open_positions_url = "https://useinsider.com/careers/open-positions/"
+  #      self.open_positions_quality_assurance_url = "https://useinsider.com/careers/open-positions/?department=qualityassurance"
 
-    # open qa careers
     def open_page(self, url):
         self.driver.get(url)
 
-    def click_button_all_qa_jobs(self):
-        if self.driver.find_element(*self.see_allQaJobs_button).is_displayed():
-            self.driver.see_allQaJobs_button.click()
-
-    def check_department_is_qa(self):
-        assert self.driver.filter_department in self.driver.filter_department("Quality Assurance").text
+    def click_button_find_jobs(self):
+        if self.driver.find_element(*self.find_jobs_button_Careers).is_displayed():
+            self.driver.find_jobs_button_Careers.click()
+        assert self.driver.current_url.find(self.open_positions_url) is not False, "Positions page has not open"
+        print("Positions page is open")
 
     def open_location_dropdown(self):
         self.driver.find_element(*self.filter_location).click()
 
     def select_location_turkey(self):
         self.driver.find_element(*self.location_Turkey).click()
+
+    def open_department_dropdown(self):
+        self.driver.find_element(*self.filter_department).click()
+
+    def select_department_quality_assurance(self):
+        self.driver.find_element(*self.department_qualityAssurance).click()
 
     def check_jobs_list(self):
         assert self.driver.find_element(*self.positions_list).is_displayed() is not False
@@ -47,3 +52,6 @@ class QualityAssurancePage:
     def check_position_location(self):
         assert (self.driver.find_element(*self.position_location)
                 in self.driver.position_location("Istanbul, Turkey").text)
+
+
+
--- pages/view_role_page.py
+++ pages/view_role_page.py
@@ -8,21 +8,21 @@ class ViewRolePage:
         self.view_role_redirect_url = (By.CSS_SELECTOR, "[content^='https://jobs.lever.co/useinsider/']")
         self.lever_logo = (By.XPATH, "//*[text()='Lever logo'")
 
+    # def hover_button_view_role(self):
+    #     self.driver.find_element(*self.view_role_button).hover()
+
+    def click_button_view_role(self):
+        print(self.driver.find_element(*self.view_role_button).__getattribute__('href').click())
+
     def open_page(self, url):
         self.driver.get(url)
 
-    def hover_button_view_role(self):
-        self.driver.find_element(*self.element_Locations)
-        assert self.element_Locations is not False
-        print("Element Locations is present on Careers page")
+    def check_view_role_redirect(self):
+        self.driver.find_element(*self.view_role_redirect_url)
+        assert self.driver.current_url.find(self.view_role_redirect_url) is not False, "Lever page has not open"
+        print("Lever page has been open")
 
-    def click_button_view_rule(self):
-        self.driver.find_element(*self.element_Careers).click()
-        assert self.driver.current_url.find(self.careers_url) is not False, "Careers page has not open"
-        print("Careers page is open")
-
-    def check_view_rule_redirect(self):
-        self.driver.find_element(*self.element_Careers).click()
-        assert self.driver.current_url.find(self.careers_url) is not False, "Careers page has not open"
-        print("Careers page is open")
+    def check_lever_logo(self):
+        self.driver.find_element(*self.lever_logo)
+        return True if self.driver.find_element(*self.lever_logo) else False
 
