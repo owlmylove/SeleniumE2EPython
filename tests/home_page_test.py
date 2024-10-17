bdffb767fc441ae7b5945267c66899c5c3bb18ef
Lilit_Gevorgyan <lilitturbizinfo@gmail.com>
2024-10-17 19:52:25 +0300

    Delete tests/home_page_test.py

--- tests/home_page_test.py
+++
@@ -1,8 +0,0 @@
-from pages.home_page import HomePage
-from tests.base_test import BaseTest
-
-
-class TestHomePage(BaseTest):
-    def test_home_page(self):
-        home_page = HomePage(self.driver)
-        home_page.check_page_loaded()
